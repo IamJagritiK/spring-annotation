ab5bb717c8e17481bb3d181839620faf3792b3d4
IamJagritiK <kumarijagrity@gmail.com>
2019-10-04 13:23:43 +0530

    Added-Task5

--- src/com/stackroute/Main.java
+++ src/com/stackroute/Main.java
@@ -1,9 +1,11 @@
 package com.stackroute;
 
 
+import com.stackroute.demo.BeanLifecycleDemoBean;
 import com.stackroute.domain.config;
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,16 +13,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class Main
+public class Main 
 {
     public static void main( String[] args )
     {
         ApplicationContext context=new AnnotationConfigApplicationContext(config.class);
+        BeanLifecycleDemoBean lc=context.getBean(BeanLifecycleDemoBean.class);
+
         Movie movie=context.getBean(Movie.class);
-         //movie.print();
+        // movie.print();
 
-
+        ((ConfigurableApplicationContext)context).close();
 
        // System.out.println( "Hello World!" );
+        
     }
 }
--- src/com/stackroute/domain/config.java
+++ src/com/stackroute/domain/config.java
@@ -1,13 +1,19 @@
 package com.stackroute.domain;
 
+import com.stackroute.demo.BeanLifecycleDemoBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-
+@Configuration
 @ComponentScan("com.stackroute.domain")
 
 public class config {
-
+@Bean
+    public BeanLifecycleDemoBean getBeanLifecycleDemoBean()
+{
+    return new BeanLifecycleDemoBean();
+}
 //    @Bean
 //    @Scope("prototype")
 //    public Movie getmovie()
